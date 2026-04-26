3832e7241889890e9f74c56ec7b35c2b3987c262
Emre Akcin <em_re-ak_cin@hotmail.com>
2026-04-27 01:15:58 +0300

    remove comment line

--- lib/features/policies/data/models/policy_dto.dart
+++ lib/features/policies/data/models/policy_dto.dart
@@ -53,7 +53,6 @@ class PolicyDto {
         );
       }
 
-      // backend sometimes sends string instead of num
       final coverageRaw = json['coverageAmount'];
       final num coverageAmount;
       if (coverageRaw is num) {
